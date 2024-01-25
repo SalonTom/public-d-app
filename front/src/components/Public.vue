79523c546c59c91470e3c034bc9141d44e46a740
Tom Salon <tom.salon62@gmail.com>
2024-01-25 18:52:15 +0100

    feat(init) : clean vue project + homepage

---
+++ front/src/components/Public.vue
@@ -0,0 +1,10 @@
+<template>
+    <img src="../assets/svg/paragraph.svg"> <span class="logo">ublic</span>
+</template>
+
+<style scoped>
+img {
+    width: calc(18px + var(--figma-ratio));
+    height: calc(18px + var(--figma-ratio));
+}
+</style>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,65 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="home-container">
+    <div class="header">
+      <Public></Public>
+    </div>
+    <div class="content-container">
+      <div style="display: flex; flex-direction: column; gap: calc(32px + var(--figma-ratio)); width: 100%;">
+        <div class="headline">
+          <div>
+            <span style="color: transparent; -webkit-text-stroke: 1px #FAFAFA;">Empower</span> your investments,
+          </div>
+          <div>
+            <span style="color: transparent; -webkit-text-stroke: 1px #FAFAFA;">Unleash</span> your projects
+          </div>
+        </div>
+        <div>
+          <div class="emphasis">The first adult-exclusive investment platform with AI-driven age recognition.</div>
+          <div style="margin-top: calc(24px + var(--figma-ratio));" class="btn btn-primary">Connect you wallet and let's go !</div>
+        </div>
+      </div>
+      <img src="../assets/eth_logo.png" style="max-width: 35vw; height: auto;">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Public from '@/components/Public.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+    Public
+  }
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.header {
+  padding: calc(20px + var(--figma-ratio)) calc(64px + var(--figma-ratio));
+  max-height: calc(80px + var(--figma-ratio));
+}
+
+.home-container {
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+  overflow-y: auto;
+}
+
+.content-container {
+  display: flex;
+  padding: 0 calc(64px + var(--figma-ratio));
+  align-items: center;
+}
+
+/* @media (max-width: 1250px) {
+  .content-container {
+    display: flex;
+    flex-direction: column;
+    padding: 0 calc(64px + var(--figma-ratio));
+    align-items: center;
+  }
+
+} */
+</style>
