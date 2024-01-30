d2d622d120f2a28f686c63943110d0ee70e9c2cb
quentingillotzebi <gillotquentin914@gmail.com>
2024-01-30 13:37:23 +0100

    push new branch before merge

--- recognition_lib/web3_utils.py
+++ recognition_lib/web3_utils.py
@@ -1,21 +1,17 @@
 import json
-from web3 import Web3
+from web3 import Web3  # Ajoutez cette ligne pour importer Web3
 
 contract = None
 
-net_url = "URL_DU_NOEUD_WEB3"
+net_url = "http://localhost:8545"  # Remplacez cela par l'URL de votre nœud Web3
 web3 = Web3(Web3.HTTPProvider(net_url))
-def connect():
-    print('a')
-
-def update_whitelist(eth_address, status):
-    print("b")
 
 def connect():
     global contract
     # Récupération du fichier abi
-    with open('chemin_vers_le_fichier_abi.json', 'r') as f:
-        abi = json.load(f)
+    with open('ProjectTokenFactory.json', 'r') as contract_file:
+        contract_data = json.load(contract_file)
+        abi = contract_data.get("abi")
 
     with open('Address.json', 'r') as address_file:
         address_data = json.load(address_file)
@@ -28,11 +24,10 @@ def update_whitelist(eth_address, status):
     global contract
 
     if contract is not None:
-        # Remplacer par la clé privée d'un compte générée par la blockchain locale
         private_key = 'YOUR_PRIVATE_KEY'
         account = web3.eth.account.privateKeyToAccount(private_key)
 
-        txn_dict = contract.functions.updateStatusWhitelist(eth_address, status).buildTransaction({
+        txn_dict = contract.functions.whitelistUser(eth_address, status).buildTransaction({
             'gas': 2000000,
             'gasPrice': web3.toWei('50', 'gwei'),
             'nonce': web3.eth.getTransactionCount(account.address),
