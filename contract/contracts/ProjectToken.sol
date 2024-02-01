abe6ba08390f23f8c19b8b6955297e0b7d523cc8
Theo-Fourniez <10551899+Theo-Fourniez@users.noreply.github.com>
2024-02-01 15:57:44 +0100

    feat: maj contrats

--- contract/contracts/ProjectToken.sol
+++ contract/contracts/ProjectToken.sol
@@ -2,22 +2,11 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "contracts/ProjectTokenFactory.sol";
 
 contract ProjectToken is ERC20 {
 
-    ProjectTokenFactory _parentFactory;
-
-    constructor(address _mintRecipient, string memory _name, string memory _symbol, uint256 _initialSupply, ProjectTokenFactory parentFactory) ERC20(_name, _symbol) {
-        require(_initialSupply < 100 && _initialSupply > 1, "The supply must be between 1 and 100");
-        _mint(_mintRecipient, _initialSupply);    
-        _parentFactory = parentFactory;
-        
-    }
-
-    modifier userWhitelisted(address userAddress){
-        require(_parentFactory.isWhitelisted(userAddress), "User is not whitelisted");
-        _;
+    constructor(address _mintRecipient, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _mint(_mintRecipient, 100 * 10**decimals()); // 18 decimals by default     
     }
 }
  
--- contract/contracts/ProjectTokenFactory.sol
+++ contract/contracts/ProjectTokenFactory.sol
@@ -22,12 +22,11 @@ contract ProjectTokenFactory {
         address owner;
         string title;
         string description;
-        uint256 initialValuation;
-        string fiatCurrency;
+        uint256 initialValuation; // Valorisation initiale en ETH
+        uint256 initialTokenNumber; // Nombre de tokens initialement mis en vente
     }
 
-    event UserWhitelisted(address u);
-    event UserPending(address u);
+    event UserWhitelistStatus(WhitelistStatus s);
 
     struct ProjectAndToken {
         ProjectStruct project;
@@ -36,9 +35,11 @@ contract ProjectTokenFactory {
 
     ProjectAndToken[] internal projectsAndTokens;
     
-    function createProject(string memory _title, string memory _description, string memory _symbol, uint256 _initialSupply, uint256 _initialValuation, string memory _fiatCurrency) public userWhitelisted(msg.sender) returns (ProjectAndToken memory){
-        ProjectStruct memory project = ProjectStruct(msg.sender, _title, _description, _initialValuation, _fiatCurrency);
-        ProjectToken token = new ProjectToken(msg.sender, _title, _symbol, _initialSupply, this);
+    // _initalValuation est la valeur en ETH transformée en entier
+    // _initialTokenNumber est la valo initale transformée
+    function createProject(string memory _title, string memory _description, string memory _symbol, uint256 _initialValuation, uint256 _initialTokenNumber) public userWhitelisted(msg.sender) returns (ProjectAndToken memory){
+        ProjectStruct memory project = ProjectStruct(msg.sender, _title, _description, _initialValuation, _initialTokenNumber);
+        ProjectToken token = new ProjectToken(msg.sender, _title, _symbol);
         ProjectAndToken memory p_t = ProjectAndToken(project, token);
         projectsAndTokens.push(p_t);
         return p_t;
@@ -70,11 +71,6 @@ contract ProjectTokenFactory {
         WhitelistStatus status
     ) public onlyAPI(msg.sender) {
         whitelist[userAddress] = status;
-        if(status == WhitelistStatus.Approved){
-            emit UserWhitelisted(userAddress);
-        }
-        else if(status == WhitelistStatus.Pending){
-            emit UserPending(userAddress);
-        }
+        emit UserWhitelistStatus(status);
     }
 }
