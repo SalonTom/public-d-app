bc5c7c982aea3752e5c41f544e822320671aa4f6
quentingillotzebi <gillotquentin914@gmail.com>
2024-02-01 14:11:15 +0100

    send signed transaction from api address

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,4 +1,4 @@
-import { ethers }from "hardhat";
+import { ethers , config }from "hardhat";
 import { Wallet, SigningKey } from "ethers";
 import { ProjectTokenFactory } from "../typechain-types";
 const path = require("path");
@@ -7,14 +7,21 @@ async function main() {
   // Types Wallet | HardhatEthersSigner 
   let apiSigner;
   
-  if((await ethers.provider.getNetwork()).name === "hardhat") {
+  if((await ethers.provider.getNetwork()).name === "hardhat" || (await ethers.provider.getNetwork()).name === "localhost") {
     console.log("Hardhat local network detected, setting the first account as API signer");
-    apiSigner = (await ethers.getSigners())[0];
+    const privateKeyHex = 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+
+    const privateKeyBytes = Buffer.from(privateKeyHex, 'hex');
+
+    const signingKey = new ethers.SigningKey(privateKeyBytes);
+
+    apiSigner = new ethers.Wallet(signingKey, ethers.provider);
   } else{
     console.log("Remote network detected, generating a random API signer");
     let signingKey = ethers.randomBytes(32);
     apiSigner = new Wallet(new ethers.SigningKey(signingKey), ethers.provider);
   }
+  console.log(apiSigner)
   const projectTokenFactory = await ethers.deployContract("ProjectTokenFactory", [apiSigner.address]);
 
   await projectTokenFactory.waitForDeployment();
@@ -22,10 +29,10 @@ async function main() {
   console.log(`Contract deployed to ${projectTokenFactory.target}, API signer is ${apiSigner.address} ${"privateKey" in apiSigner ? ` with private key ${apiSigner.privateKey}` : ''}`);
 
   // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(projectTokenFactory);
+  saveFrontendFiles(projectTokenFactory,apiSigner);
 }
 
-function saveFrontendFiles(projectTokenFactory: ProjectTokenFactory) {
+function saveFrontendFiles(projectTokenFactory: ProjectTokenFactory,apiSigner) {
   const fs = require("fs");
   const contractsDir = path.join(__dirname, "..", "contracts");
 
@@ -44,6 +51,11 @@ function saveFrontendFiles(projectTokenFactory: ProjectTokenFactory) {
     path.join(contractsDir, "ProjectTokenFactory.json"),
     JSON.stringify(ProjectTokenFactoryArtifact, null, 2)
   );
+
+
+  fs.writeFileSync(
+      path.join(contractsDir, "API.json"),
+      JSON.stringify({ api_address: apiSigner.address, private_key : apiSigner.privateKey},null,2));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- recognition_lib/web3_utils.py
+++ recognition_lib/web3_utils.py
@@ -1,14 +1,18 @@
 import json
+
+from eth_account import Account
 from web3 import Web3  # Ajoutez cette ligne pour importer Web3
 
 contract = None
 
 net_url = "http://localhost:8545"  # Remplacez cela par l'URL de votre nœud Web3
-web3 = Web3(Web3.HTTPProvider(net_url))
-
+w3 = Web3(Web3.HTTPProvider(net_url))
 
 def connect():
     global contract
+    global api_address
+    global private_key
+
     # Récupération du fichier abi
     with open('../shared_contracts_info/ProjectTokenFactory.json', 'r') as contract_file:
         contract_data = json.load(contract_file)
@@ -18,11 +22,25 @@ def connect():
         address_data = json.load(address_file)
         address = address_data.get("Address")
 
-    contract = web3.eth.contract(address=address, abi=abi)
+    with open('../shared_contracts_info/API.json', 'r') as api_file:
+        api_data = json.load(api_file)
+        api_address = api_data.get("api_address")
+        private_key = api_data.get("private_key")
+    contract = w3.eth.contract(address=address, abi=abi)
 
+def update_whitelist(eth_address, status) :
+    Chain_id = w3.eth.chain_id
+    global private_key
+    global api_address
 
-def update_whitelist(eth_address, status):
-    global contract
-    if contract is not None:
-        call_function = contract.functions.whitelistUser(eth_address, status).transact()
-        print("appel passé")
+    nonce = w3.eth.get_transaction_count(api_address)
+
+    call_function = contract.functions.whitelistUser(eth_address, status).build_transaction(
+        {"chainId": Chain_id, "from": api_address, "nonce": nonce})
+
+    signed_tx = w3.eth.account.sign_transaction(call_function, private_key=private_key)
+
+    send_tx = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+
+    tx_receipt = w3.eth.wait_for_transaction_receipt(send_tx)
+
